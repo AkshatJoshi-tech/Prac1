f89ebc3479aeb362b59ca3f924b9c1c3054e0523
Akshat Joshi <akshatjoshi@thoughtwin.com>
2025-04-15 19:09:02 +0530

    commit 4

--- index.js
+++ index.js
@@ -1,4 +1,14 @@
-//REmove Duplicate  i takes questions and then solved
-let arr = [1, 2, 2, 3, 4, 4, 5];
-let removeDuplicate = new Set(arr);
-console.log(removeDuplicate);
+const express= require('express');
+const cors = require('cors');
+const mongoose=require('mongoose');
+const app=express();
+app.use(cors())
+
+app.get('/',(req,res)=>{
+    res.send("Hello")
+    console.log("hello")
+})
+
+app.listen(5000,()=>{
+    console.log(`server started on port 5000`)
+})
